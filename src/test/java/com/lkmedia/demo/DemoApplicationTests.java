bbd3dae6abf62b513181f3690d830bbd94f30592
ljo <jongohlee71@gmail.com>
2023-12-10 20:40:24 +0900

    첫 커밋

---
+++ src/test/java/com/lkmedia/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lkmedia.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
